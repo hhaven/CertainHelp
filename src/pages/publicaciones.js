df8aec3048147e535de8cedddb4d87dc5712f5cc
hhaven <54193283+hhaven@users.noreply.github.com>
2022-10-19 14:28:16 -0600

    Arreglo a publicaciones

--- src/pages/publicaciones.js
+++ src/pages/publicaciones.js
@@ -6,18 +6,28 @@ import { CarouselPubs } from '../components/Carousel';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
+import { Form } from 'react-bootstrap';
 
 export const Publicaciones = () => {
 
   return (
 
     <Container>
-      <Row>
-        <CarouselPubs/>
-      </Row>
-      <Row>
-        <GridCards/>
-      </Row>
+        <Row>
+                <Form.Label htmlFor="inputPassword5">Buscar</Form.Label>
+                <Form.Control
+                    aria-describedby="passwordHelpBlock"
+                    
+                />
+                <Form.Text id="passwordHelpBlock" muted className = 'customCarousel'>
+                </Form.Text>
+        </Row>
+        <Row>
+            <CarouselPubs/>
+        </Row>
+        <Row>
+            <GridCards/>
+        </Row>
     </Container>
 
   );
